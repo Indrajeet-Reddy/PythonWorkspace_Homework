ffed7250814b50f9fe317ba24f7ef5888006914f
Indrajeet-Reddy <indrajeetreddy12@gmail.com>
2026-05-19 18:41:10 +0530

    Add files via upload

---
+++ S_Slicing.py
@@ -0,0 +1,17 @@
+a = "Indrajeet Reddy"
+# Name 
+print(a[0:15])
+print(a[0:9])
+print(a[10:15])
+
+print(a[14:9:-1])
+
+print(a[-7:0:-1])
+
+print(a[0:-6:2])
+
+print(a[8:-15:-4])
+
+print(a[-15:-6])
+
+print(a[-1:-16:-1])
